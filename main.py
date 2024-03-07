6d31ac8a72cfe92c6865765219638c2b2c3ef83e
Horace Pan <horace@gauntlet.network>
2024-03-07 10:07:23 -0800

    vscode format

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 from dataclasses import dataclass
 from typing import Any
 
+import attrs
 import libcst as cst
 from libcst.metadata import PositionProvider
 
@@ -58,11 +59,13 @@ class EvalExecVisitor(StyleViolationsVisitor):
             "getattr",
             "setattr",
         ]:
-            self.violations.append(StyleViolation(
-                name="Invalid function used",
-                line_number=start_line_num,
-                message=f"Dangerous call found: {node.func.value}",
-            ))
+            self.violations.append(
+                StyleViolation(
+                    name="Invalid function used",
+                    line_number=start_line_num,
+                    message=f"Dangerous call found: {node.func.value}",
+                )
+            )
 
 
 class NestedFunctionVisitor(StyleViolationsVisitor):
@@ -78,12 +81,15 @@ class NestedFunctionVisitor(StyleViolationsVisitor):
         if len(self.function_stack) > 0:
             parent_name = self.function_stack[-1].name.value
             msg = f"Function {fname} defined within {parent_name}"
-            self.violations.append(StyleViolation("Nested function", start_line_num, msg))
+            self.violations.append(
+                StyleViolation("Nested function", start_line_num, msg)
+            )
 
         self.function_stack.append(node)
 
     def leave_FunctionDef(self, node: cst.FunctionDef):
         self.function_stack.pop()
+
 
 class FunctionArgAssignVisitor(StyleViolationsVisitor):
     def __init__(self):
@@ -104,11 +110,13 @@ class FunctionArgAssignVisitor(StyleViolationsVisitor):
             if assign_target in current_function.arg_names:
                 fn_name = current_function.name
                 value = value_from_assign_target(target)
-                self.violations.append(StyleViolation(
-                    name="Assign to function arg",
-                    line_number=start_line_num,
-                    message=f"Function {fn_name} arg {value} is being modified"
-                ))
+                self.violations.append(
+                    StyleViolation(
+                        name="Assign to function arg",
+                        line_number=start_line_num,
+                        message=f"Function {fn_name} arg {value} is being modified",
+                    )
+                )
 
     def visit_FunctionDef(self, node: cst.FunctionDef):
         code_range = self.get_metadata(PositionProvider, node)
@@ -131,17 +139,56 @@ class LambdaVisitor(StyleViolationsVisitor):
     def visit_Lambda(self, node: cst.Lambda):
         code_range = self.get_metadata(PositionProvider, node)
         start_line_num = code_range.start.line
-        self.violations.append(StyleViolation(
-            name="Lambda",
-            line_number=start_line_num,
-            message=f"Lambda found at line: {start_line_num}",
-        ))
+        self.violations.append(
+            StyleViolation(
+                name="Lambda",
+                line_number=start_line_num,
+                message=f"Lambda found at line: {start_line_num}",
+            )
+        )
+
+
+class AttrDecoratorVisitor(StyleViolationsVisitor):
+    """
+    two attr decorators allowed:
+    @attr.s(auto_attribs=True, frozen=True)
+
+    and
+    @attr.s(auto_attribs=True)
+    """
+
+    # def visit_ClassDef_decorators(self, node: cst.Attribute):
+    #     code_range = self.get_metadata(PositionProvider, node)
+    #     start_line_num = code_range.start.line
+
+    #     self.violations.append(
+    #         StyleViolation(
+    #             name="Attribute",
+    #             line_number=start_line_num,
+    #             message=f"Attribute found at line: {start_line_num}",
+    #         )
+    #     )
+
+    def visit_ClassDef(self, node: cst.ClassDef) -> None:
+        # Check for @attr.s decorator
+        for decorator in node.decorators:
+            if isinstance(decorator.decorator, cst.Call) and isinstance(decorator.decorator.func, cst.Name):
+                decorator_name = decorator.decorator.func.value
+                if decorator_name == "attr.s":
+                    self._check_attrs_usage(decorator.decorator, node)
+
 
 
 def run_evals(code: str):
     code_lines = code.split("\n")
     tree = cst.parse_module(code)
-    visitors = [EvalExecVisitor(), NestedFunctionVisitor(), LambdaVisitor(), FunctionArgAssignVisitor()]
+    visitors = [
+        EvalExecVisitor(),
+        NestedFunctionVisitor(),
+        LambdaVisitor(),
+        FunctionArgAssignVisitor(),
+        AttrDecoratorVisitor(),
+    ]
     wrapper = cst.MetadataWrapper(tree)
 
     for visitor in visitors:
@@ -150,6 +197,7 @@ def run_evals(code: str):
     for visitor in visitors:
         for violation in visitor.violations:
             print(f"{violation} | Line: {code_lines[violation.line_number - 1]}")
+
 
 if __name__ == "__main__":
     source = """
@@ -164,6 +212,12 @@ def a():
         return 10
     return b()
 eval("1+1")
+
+
+@attrs.s(auto_attribs=True, frozen=True)
+class MyData:
+    x: int
+    y: str
 """
     # visitor = check_style(source)
     run_evals(source)

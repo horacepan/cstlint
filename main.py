14777f273d2e076bd4940f926de27d67676184c8
Horace Pan <horace@gauntlet.network>
2024-03-06 23:30:57 -0800

    assign check on function args working

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+from dataclasses import dataclass
+from typing import Any
 
 import libcst as cst
 from libcst.metadata import ParentNodeProvider
@@ -8,22 +10,84 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+@dataclass
+class FunctionInfo:
+    name: str
+    arg_names: list[Any]
+
+    def contains_arg(self, arg: str) -> bool:
+        return arg in self.args
+
+
+def value_from_assign_target(target: cst.AssignTarget) -> str:
+    # TODO: maybe a better way to extract the underlying
+    # var name/value out of an AssingTarget node
+    if isinstance(target.target, cst.Name):
+        return target.target.value
+    elif isinstance(target.target, cst.Subscript):
+        return target.target.value.value
+    elif isinstance(target.target, cst.Attribute):
+        return target.target.value.value
+    raise ValueError(f"Unsupported assign target type: {target}")
+
+
 class StyleGuideCheckerVisitor(cst.CSTVisitor):
     METADATA_DEPENDENCIES = (ParentNodeProvider, PositionProvider)
 
-    def __init__(self):
+    def __init__(self, source_code: str):
         super().__init__()
+        self.source_code_lines = source_code.splitlines()
         self.inline_functions = []
+        self.args = {}
+        self.assigned_targets = []
+        self.function_stack = []
+        self.function_args = []
+
+    def visit_Assign_value(self, node: cst.Assign):
+        """
+        RHS of an assign statement
+        """
+        pass
+
+    def visit_Assign_targets(self, node: cst.Assign):
+        """
+        Detect if a variable being assigned to as one of the arguments
+        """
+        code_range = self.get_metadata(PositionProvider, node)
+        start_line_num = code_range.start.line
+
+        for target in node.targets:
+            assign_target = value_from_assign_target(target)
+            self.assigned_targets.append(assign_target)
+
+            if assign_target in self.function_stack[-1].arg_names:
+                fn_name = self.function_stack[-1].name
+                value = value_from_assign_target(target)
+                logger.info(
+                    f"Function {fn_name} arg {value} is being modified: {self.source_code_lines[start_line_num-1]}"
+                )
 
     def visit_FunctionDef(self, node: cst.FunctionDef):
         parent = self.get_metadata(ParentNodeProvider, node)
         code_range = self.get_metadata(PositionProvider, node)
         start_line_num = code_range.start
+        fn_args = []
+        for param in node.params.params:
+            fn_args.append(param.name.value)
 
         # Just checks that the parent is is a function too
         # TODO: check ancestors
         if isinstance(parent, cst.FunctionDef):
             logger.info(f"Inline function found at line: {start_line_num}")
+
+        fn_info = FunctionInfo(
+            name=node.name.value,
+            arg_names=fn_args,
+        )
+        self.function_stack.append(fn_info)
+
+    def leave_FunctionDef(self, node: cst.FunctionDef):
+        self.function_stack.pop()
 
     def visit_Lambda(self, node: cst.Lambda):
         code_range = self.get_metadata(PositionProvider, node)
@@ -35,17 +99,17 @@ class StyleGuideCheckerVisitor(cst.CSTVisitor):
 def check_style(code: str):
     tree = cst.parse_module(code)
     wrapper = cst.MetadataWrapper(tree)
-    visitor = StyleGuideCheckerVisitor()
+    visitor = StyleGuideCheckerVisitor(code)
     wrapper.visit(visitor)
     return visitor
 
 
 if __name__ == "__main__":
     source = """
-def outer_function():
-    x = lambda z: z + 10
-    return x(5)
-
-z = lambda t: t+10
+def set_value(a, b, c):
+    a = 10
+    b[10] = 0
+    c.x = 100
+    d[0] += 30
 """
     visitor = check_style(source)
